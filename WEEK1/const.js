b42bdc1c4c204eb95c6bb3fe9dcb78544cc409e1
Jaewoo2468 <ijaeu23@gmail.com>
2023-09-12 12:10:07 -0400

    first commit

---
+++ WEEK1/const.js
@@ -0,0 +1,5 @@
+const me = 1;
+//
+
+me = 2; //cannot reinitialize
+console.log(me);
---
+++ WEEK1/main.js
@@ -0,0 +1,10 @@
+//import { flag, touch } from './validator.js';
+//console.log(flag);
+//touch();
+//console.log(flag);
+//import {halfOf, multiply} from './lib.js';
+//console.log(halfOf(84));
+//console.log(multiply(5,2));
+
+import doSomething from './doSomething';
+doSomething();
